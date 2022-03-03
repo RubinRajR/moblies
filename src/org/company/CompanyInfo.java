4709f9264792044c9da063399b1c484d4cfed549
RubinRajR <rubinrao12005@gmail.com>
2022-03-03 12:37:41 +0530

    folder

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,24 @@
+package org.company;
+
+public class CompanyInfo {
+
+	private void companyName() {
+System.out.println("tvs");
+	}
+	private void companyId() {
+System.out.println("12345");
+	}
+	private void companyAddress() {
+System.out.println("12/2 sivan kovil street t nagar");
+	}
+	
+	public static void main(String[] args) {
+		CompanyInfo e = new CompanyInfo();
+		
+		e.companyName();
+		e.companyId();
+		e.companyAddress();
+		
+	}
+	
+}
